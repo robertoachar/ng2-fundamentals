b433783436a5bd44bbadd9d891ab6be9259735f0
Roberto Achar <robertoachar@gmail.com>
2017-01-21 16:51:54 -0200

    Styling components

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -3,13 +3,9 @@ import { Component, Input } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'event-thumbnail',
-  templateUrl: 'event-thumbnail.component.html'
+  templateUrl: 'event-thumbnail.component.html',
+  styleUrls: ['event-thumbnail.component.css']
 })
 export class EventThumbnailComponent {
   @Input() event: any;
-  someProperty: any = 'some value';
-
-  logFoo() {
-    console.log('Foo');
-  }
 }
