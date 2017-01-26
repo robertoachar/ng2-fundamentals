4995cf9dbd5fd06ffd633adaa92f3a7689763e18
Roberto Achar <robertoachar@gmail.com>
2017-01-26 20:45:21 -0200

    Testing Components with Deep Integrated Tests with Mock Services

--- app/events/session-list.component.integrated.spec.ts
+++ app/events/session-list.component.integrated.spec.ts
@@ -2,11 +2,13 @@ import { DebugElement } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 
+import { ISession } from './event.model';
 import { AuthService } from '../user/auth.service';
 import { VoterService } from './voter.service';
-
 import { SessionListComponent } from './session-list.component';
-import { ISession } from './event.model';
+import { UpvoteComponent } from './upvote.component';
+import { DurationPipe } from './duration.pipe';
+import { CollapsibleWellComponent } from '../common/collapsible-well.component';
 
 describe('SessionListComponent', () => {
   let fixture: ComponentFixture<SessionListComponent>;
@@ -15,13 +17,21 @@ describe('SessionListComponent', () => {
   let debugEl: DebugElement;
 
   beforeEach(async(() => {
-    let mockAuthService = {};
-    let mockVoterService = {};
+    let mockAuthService = {
+      isAuthenticated: () => true,
+      currentUser: { username: 'Joe' }
+    };
+    let mockVoterService = {
+      userHasVoted: () => true
+    };
 
     TestBed.configureTestingModule({
       imports: [],
       declarations: [
-        SessionListComponent
+        SessionListComponent,
+        UpvoteComponent,
+        DurationPipe,
+        CollapsibleWellComponent
       ],
       providers: [
         { provide: AuthService, useValue: mockAuthService },
@@ -36,5 +46,31 @@ describe('SessionListComponent', () => {
     component = fixture.componentInstance;
     debugEl = fixture.debugElement;
     element = fixture.nativeElement;
+  });
+
+  describe('initial display', () => {
+
+    it('should have the correct session title', () => {
+      component.sessions = <ISession[]>[
+        {
+          id: 3,
+          name: 'Session 1',
+          presenter: 'Joe',
+          duration: 1,
+          level: 'beginner',
+          abstract: 'abstract',
+          voters: ['john', 'bob']
+        }
+      ];
+      component.filterBy = 'all';
+      component.sortBy = 'name';
+      component.eventId = 4;
+
+      component.ngOnChanges();
+
+      fixture.detectChanges();
+
+      expect(element.querySelector('[well-title]').textContent).toContain('Session 1');
+    });
   });
 });
