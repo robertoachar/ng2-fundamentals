fde0500f1025c20e5ac2e66e186d3f7e6ed20740
Roberto Achar <robertoachar@gmail.com>
2017-01-26 19:50:49 -0200

    Testing Mock Calls

--- app/events/voter.service.spec.ts
+++ app/events/voter.service.spec.ts
@@ -13,6 +13,18 @@ describe('VoterService', () => {
     voterService = new VoterService(mockHttp);
   });
 
+  describe('addVoter', () => {
+    it('should call http.post with the right url', () => {
+      let session = { id: 6, voters: ['john'] };
+
+      mockHttp.post.and.returnValue(Observable.of(false));
+
+      voterService.addVoter(3, <ISession>session, 'joe');
+
+      expect(mockHttp.post).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe', Object({}), jasmine.any(Object));
+    });
+  });
+
   describe('deleteVoter', () => {
 
     it('should remove the voter from the list of voters', () => {
@@ -24,6 +36,16 @@ describe('VoterService', () => {
 
       expect(session.voters.length).toBe(1);
       expect(session.voters[0]).toBe('john');
+    });
+
+    it('should call http.delete with the right url', () => {
+      let session = { id: 6, voters: ['joe', 'john'] };
+
+      mockHttp.delete.and.returnValue(Observable.of(false));
+
+      voterService.deleteVoter(3, <ISession>session, 'joe');
+
+      expect(mockHttp.delete).toHaveBeenCalledWith('/api/events/3/sessions/6/voters/joe');
     });
   });
 });
